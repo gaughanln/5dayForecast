f6b292cb7f0474b2bf1fd61ead93a5694699bbab
gaughanln <gaughanln@yahoo.com>
2023-01-05 14:03:43 -0600

    got the fetch working

--- assets/style.js
+++ assets/style.js
@@ -1,64 +1,31 @@
 // GIVEN a weather dashboard with form inputs
 
 // GLOBAL VARIABLES
-const key = "a411ef0030322e0862cd44cde300dd84";
-var citySearchBoxEl = $("#city-search");
-var formInputEl = $(".form-input")
-var pastSearchEl = $("#past-search");
-var searchBoxEl = document.getElementById(".search-box");
-var searchBtn = document.querySelector(".btn");
-
-searchBtn.addEventListener("click", function () {
-  console.log(this.innerHTML);
-});
+// const key = "a411ef0030322e0862cd44cde300dd84";
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=a411ef0030322e0862cd44cde300dd84";
+// reference activity 24 for this 
+var searchBoxEl = document.getElementById("search-box");
+var citySearchEl = document.getElementsByClassName("city-search");
+var formInputEl = document.getElementsByClassName("form-input");
+var searchBtnEl = document.getElementsByClassName("btn");
+var pastSearchEl = document.getElementById("past-search")
 
 
-$(document).ready(function () {
-$(".btn").click(function() {
+// fetched API
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
-var searchBoxEl = $(this).parent.attr("id");
+// searchBtn.addEventListener("click", function () {
+//   console.log(this.innerHTML);
+// });
 
-var cityEl = $(this).prev("input").val();
 
-localStorage.setItem(searchBoxEl, cityEl);
 
-});
-})
 
-// line 29 is not proper syntax. we don't want the info to stay in the box,
-$(document).ready(function () {
-$("#search-box").each(function (){
 
-var searchBoxEl =$(this).attr("id");
 
-var savedSearch = localStorage.getItem(searchBoxEl);
-$(this).find("input").val(savedSearch)
-});
-})
 
-// function showPastSearch(event) {
-//   event.preventDefault();
-
-//   var city = $('input[name="city-input"]').val();
-
-//   if (!city) {
-//     console.log('No searches have been made');
-//     return;
-//   }
-
-//   var cityName = $('<li class="flex-row justify-space-between align-center p-2 text-dark">');
-  
-//   cityName.text(city);
-
-//   // print to the page
-//   pastSearchEl.append(cityName);
-
-//   // clear the form input element
-//   $('input[name="city-input"]').val('');
-// }
-
-// API Key
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={a411ef0030322e0862cd44cde300dd84}
 
 
 
@@ -82,4 +49,4 @@ $(this).find("input").val(savedSearch)
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 
 // WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+// THEN I am again presented with current and future conditions for that city. needs to be like a button that will bring back the search
